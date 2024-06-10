1bf850f0eb6329f7571438d4ea27cdf3a8d958a8
Sulav Baniya <baniyasulav@gmail.com>
2024-06-10 19:01:56 +0545

    modified about us

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// script.js
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwlnN-TSiv0iX2Mu67MkLLBqgK3jv3zi7cxx4A7vivzCk1PX2ewSFEgijgNbGZ4xmCPig/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Add your JavaScript here if needed
-});
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+          msg.innerHTML =""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+
